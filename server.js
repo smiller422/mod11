68aa1232354317882d3148195ecc5698dd0eeea2
Sarah~ <s.k.miller422@gmail.com>
2023-04-27 07:46:56 -0400

    updated server.js file

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const fs = require("fs")
 // const data = require('.. /../../db/db.json') //needs space after first set of double dots
-const data = require('./db/db.json')
+const dataPath = require('./db/db.json')
+const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'))
+
 console.log(data)
 
 // const PORT = 3001;
@@ -23,29 +25,40 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html')) //changed index.html to notes.html 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')) //changed index.html to notes.html 
 );
 
 // GET Route for feedback page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) =>
-{console.log("youre hitting api notes")
-  res.json(data)}
-);
-app.post('/api/notes', (req, res) =>
-{ let note = req.body 
-  data.push(note)
-  fs.writeFile('../Develop/db/db.json', JSON.stringify(data), (error) => {
-    if (error) throw error;
+app.get('/api/notes', (req, res) => {
+  console.log('youre hitting api notes');
+  const updatedData = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+  res.json(updatedData);
+});
+
+app.post('/api/notes', (req, res) => {
+  let note = req.body;
+
+  // Read notes data from file
+  fs.readFile(dataPath, 'utf8', (err, notesData) => {
+    if (err) throw err;
+    let parsedNotesData = JSON.parse(notesData);
+
+    // Add a new note
+    note.id = parsedNotesData.length + 1;
+    parsedNotesData.push(note);
+
+    // Update notes data in the file
+    fs.writeFile(dataPath, JSON.stringify(parsedNotesData), (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+
+      // Send the updated notes data as a response
+      res.json(parsedNotesData);
+    });
   });
-  }
-);
+});
 
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`));
